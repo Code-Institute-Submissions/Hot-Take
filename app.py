da977bb3c1aae506fe3eafdbee2f278931b61a6c
JBlk99 <joshualeebernard@gmail.com>
2021-06-10 23:50:48 +0000

    Added base pages for site and css to the base template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -17,26 +18,37 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-@app.route('/index')
-@app.route('/albums')
-def get_albums():
+@app.route("/")
+def index():
+    return render_template("index.html", page_title="Welcome To Hot Take")
+
+
+@app.route("/albums")
+def albums():
     albums = mongo.db.album_data.find()
-    return render_template("albums.html", albums=albums)
+    return render_template("albums.html", albums=albums, page_title="Albums")
 
 
-@app.route('/profile')
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html", page_title="Register")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", page_title="Profile")
 
 
-@app.route('/contact_us')
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", page_title="Contact Us")
+
+@app.route("/sign_in")
+def sign_in():
+    return render_template("sign-in.html", page_title="Sign in")
 
 
-@app.route('/sign_in')
 
-
-@app.route('/')
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
